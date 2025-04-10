a154b412ceb3fc36144b10286a608720afd4b7c8
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-04-10 12:16:57 -0300

    [DEV-142} Refactor: Create service for Redis Subscriber Thread

---
+++ app/services/redis_subscriber_service.rb
@@ -0,0 +1,24 @@
+require 'redis'
+class RedisSubscriberService
+  def self.start(channel:, connections:)
+    Thread.new do
+      subscriber = Redis.new(host: 'redis', port: 6379) # Conexão separada para subscrição
+      loop do
+        begin
+          puts "🔄 Subscrição ao canal Redis iniciada..."
+          subscriber.subscribe(channel) do |on|
+            on.message do |_channel, message|
+              connections_mutex.synchronize do
+                connections.each { |ws| ws.send(message) if ws.ready_state == Faye::WebSocket::OPEN }
+              end
+            end
+          end
+        rescue => e
+          puts "⚠️ Erro no Redis: #{e.message}. Tentando reconectar..."
+          sleep 2
+          retry
+        end
+      end
+    end
+  end
+end
