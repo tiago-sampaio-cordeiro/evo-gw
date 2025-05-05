5b19e20e5210a92077fc89ed6c79488023f8f00f
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-05 14:47:05 -0300

    [DEV-155] Feature: adicionando resposta ao comando sendlog

--- app/services/devices/evo.rb
+++ app/services/devices/evo.rb
@@ -20,6 +20,20 @@ module Devices
     when "sendlog"
       puts "Logs recebidos do dispositivo: #{message['sn']}"
       puts "Total de logs: #{message['count']}"
+
+      response = {
+        ret: 'sendlog',
+        result: true,
+        count: message['count'],
+        logindex: message['logindex'],
+        cloudtime: Time.now.utc.iso8601,
+        access: 1,
+        message: 'Logs recebidos com sucesso'
+      }
+
+      ws.send(response.to_json)
+      puts "Resposta enviada ao dispositivo:"
+      puts JSON.pretty_generate(response)
     end
   end
 end
