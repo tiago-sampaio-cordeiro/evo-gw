6c94add68690bcc4a3182af18e4dd579d34842ec
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-07 12:02:44 -0300

    [DEV-155] Feature: Add methods

--- app/services/devices/sender.rb
+++ app/services/devices/sender.rb
@@ -24,13 +24,92 @@ module Devices
     def self.set_user_info(ws, user)
       command = {
         cmd: 'setuserinfo',
-        enrollid: 1,
-        name: "Pablo P",
+        enrollid: user,
+        name: "Pablo",
         backupnum: 0
       }
 
       ws.send(command.to_json)
       puts "commando 'setuserinfo' enviado para o aparelho"
     end
+
+    # servidor retorna true para a operação, mas o user não é deletado do aparelho
+    def self.delete_user(ws, user)
+      command = {
+        cmd: 'deleteuser',
+        enrollid: user,
+        backupnum: 0
+      }
+
+      ws.send(command.to_json)
+      puts "comando 'deleteuser' enviado para o aparelho"
+    end
+
+    def self.get_username(ws, user)
+      command = {
+        cmd: 'getusername',
+        enrollid: user
+      }
+
+      ws.send(command.to_json)
+      puts "Comando 'getusername' enviado para o aparelho"
+    end
+
+    # def self.setUsername(ws, user)
+    #   command = {
+    #     cmd: 'setusername',
+    #     count: , # numero de usuarios que sera setado o nome
+    #     record: [
+    #       {
+    #         enrollid: user,
+    #         name: "Teste"
+    #       }
+    #     ]
+    #   }
+    # end
+
+    def self.enable_user(ws, user)
+      command = {
+        cmd: 'enableuser',
+        enrollid: user,
+        enflag: 1 # valor pode ser 0 ou 1, 0 para desabilitar e 1 para habilitar
+      }
+
+      ws.send(command.to_json)
+      puts "Comando 'enableuser' enviado para o aparelho"
+    end
+
+    # Remove todos os usuarios do equipamento
+    def self.clean_user(ws)
+      command = {
+        cmd: 'cleanuser'
+      }
+
+      ws.send(command.to_json)
+      puts "Comando 'enableuser' enviado para o aparelho"
+    end
+
+    def self.get_all_log(ws, initi_data, end_data)
+      command = {
+        cmd: 'getalllog',
+        stn: true,
+        from: initi_data, # Data inicial
+        to: end_data # Data final
+      }
+
+      ws.send(command.to_json)
+      puts "Comando 'getalllog' enviado para o aparelho"
+    end
+
+    def self.clean_log(ws)
+      command = {
+        cmd: 'cleanlog'
+      }
+
+      ws.send(command.to_json)
+      puts "Commando 'cleanlog' enviado para o aparelho"
+    end
+
+
   end
 end
--- server.rb
+++ server.rb
@@ -58,8 +58,74 @@ class Server < Rack::App
     ws = self.class.instance_variable_get(:@connections).first
 
     if ws
-      Devices::Sender.set_user_info(ws, 1)
-      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado userInfo' }.to_json]]
+      Devices::Sender.set_user_info(ws, 2)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado SetuserInfo' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  post '/delete_user' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.delete_user(ws, 1)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado DeleteUser' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  get '/username' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.get_username(ws, 1)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado getUsername' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  post '/enable_user' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.enable_user(ws, 1)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado enableUser' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  post 'clean_user' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.clean_user(ws, 1)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado cleanUser' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  get 'get_all_log' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.get_all_log(ws, 2025-01-01, )
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado getAllLog' }.to_json]]
+    else
+      [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
+    end
+  end
+
+  post 'clean_log' do
+    ws = self.class.instance_variable_get(:@connections).first
+
+    if ws
+      Devices::Sender.clean_log(ws)
+      [200, { 'Content-Type' => 'application/json' }, [{ status: 'comando enviado cleanLog' }.to_json]]
     else
       [500, { 'Content-Type' => 'application/json' }, [{ error: 'Nenhuma conexão WebSocket ativa' }.to_json]]
     end
