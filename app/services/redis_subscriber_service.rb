0b48a18e3b11fc2239ace4efcf17652e567da8d3
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-15 11:53:05 -0300

    [DEV-170] REFACTOR: adicionado novos logs de erros

--- app/services/redis_subscriber_service.rb
+++ app/services/redis_subscriber_service.rb
@@ -1,7 +1,7 @@
 require 'logger'
 
 class RedisSubscriberService
-  Logger = Logger.new($stdout)
+  LOGGER = Logger.new($stdout)
 
   @subscribed_channels = {}
   @mutex = Mutex.new
@@ -10,28 +10,39 @@ class RedisSubscriberService
     @mutex.synchronize { @subscribed_channels.key?(channel) }
   end
 
-  def self.start(channel:, connections:, mutex:)
+  def self.start(channel:, ws:, mutex:, logger:)
     return if subscribed?(channel)
 
     @mutex.synchronize { @subscribed_channels[channel] = true }
 
     Thread.new do
-      subscriber = Redis.new(host: 'redis', port: 6379)
-      loop do
-        begin
-          Logger.info "🔄 Subscrição ao canal Redis '#{channel}' iniciada..."
-          subscriber.subscribe(channel) do |on|
-            on.message do |_chan, message|
-              mutex.synchronize do
-                connections.each { |ws| ws.send(message) }
-              end
+      begin
+        subscriber = Redis.new(host: 'redis', port: 6379)
+
+        LOGGER.info "🔄 Subscrição ao canal Redis '#{channel}' iniciada..."
+
+        subscriber.subscribe(channel) do |on|
+          on.message do |_chan, message|
+            mutex.synchronize do
+              ws.send(message)
             end
           end
-        rescue => e
-          Logger.info "⚠️ Erro no Redis (canal: #{channel}): #{e.message}. Tentando reconectar..."
-          sleep 2
-          retry
         end
+
+
+      rescue Redis::CannotConnectError => e
+        LOGGER.error "⚠️ Falha ao conectar no Redis (canal: #{channel}): #{e.message}. Tentando reconectar..."
+        sleep 2
+        retry
+
+      rescue => e
+        LOGGER.error "⚠️ Erro no Redis (canal: #{channel}): #{e.message}. Tentando reconectar..."
+        sleep 2
+        retry
+
+      ensure
+        @mutex.synchronize { @subscribed_channels.delete(channel) }
+        LOGGER.info "❌ Subscrição ao canal '#{channel}' foi encerrada."
       end
     end
   end
