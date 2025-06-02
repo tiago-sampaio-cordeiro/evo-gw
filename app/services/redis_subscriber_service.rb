198beeaaca6218eb6569643a2e1be2532d38e914
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-06-02 11:17:11 -0300

    [DEV-174]ENHANCEMENT: mutex exclusivo para redis

--- app/services/redis_subscriber_service.rb
+++ app/services/redis_subscriber_service.rb
@@ -2,58 +2,52 @@ require 'logger'
 
 class RedisSubscriberService
 
-  @subscribed_channels = {}
-  @mutex = Mutex.new
+  def self.start(channel:, ws:, mutex_subscribed_channels:, logger:, subscribed_channels:)
+    return if mutex_subscribed_channels.synchronize { subscribed_channels.key?(channel) }
 
-  def self.subscribed?(channel)
-    @mutex.synchronize { @subscribed_channels.key?(channel) }
-  end
-
-  def self.start(channel:, ws:, mutex:, logger:)
-    return if subscribed?(channel)
-
-    mutex.synchronize { subscribed_channels[channel] = true }
+    mutex_subscribed_channels.synchronize { subscribed_channels[channel] = true }
 
     Thread.new do
       begin
         subscriber = Redis.new(host: 'redis', port: 6379)
 
-        logger.info "🔄 Subscrição ao canal Redis '#{channel}' iniciada..."
+        logger.info "Subscrição ao canal Redis '#{channel}' iniciada..."
 
         subscriber.subscribe(channel) do |on|
           on.message do |_chan, message|
             begin
+              logger.debug "Mensagem recebida no canal '#{channel}': #{message}"
+
               payload = JSON.parse(message)
               command = payload['cmd']
               args = payload['args'] || []
 
               if command
-                mutex.synchronize do
+                mutex_subscribed_channels.synchronize do
                   Devices::Sender.send(ws, command, *args)
                 end
               else
-                logger.warn "⚠️ Payload recebido sem comando no canal '#{channel}': #{payload}"
+                logger.warn "Payload recebido sem comando no canal '#{channel}': #{payload}"
               end
             rescue JSON::ParserError => e
-              logger.error "❌ Erro ao fazer parse do JSON no canal '#{channel}': #{e.message}"
+              logger.error "Erro ao fazer parse do JSON no canal '#{channel}': #{e.message}"
             rescue => e
-              logger.error "❌ Erro ao processar comando no canal '#{channel}': #{e.message}"
+              logger.error "Erro ao processar comando no canal '#{channel}': #{e.message}"
             end
           end
         end
 
       rescue Redis::CannotConnectError => e
-        @mutex.synchronize { @subscribed_channels.delete(channel) }
-        logger.error "⚠️ Falha ao conectar no Redis (canal: #{channel}): #{e.message}. Cancelando subscrição."
+        logger.error "Falha ao conectar ao Redis (canal: #{channel}): #{e.message}"
 
       rescue => e
-        logger.error "⚠️ Erro no Redis (canal: #{channel}): #{e.message}. Tentando reconectar..."
+        logger.error "Erro no Redis (canal: #{channel}): #{e.message}. Tentando reconectar..."
         sleep 2
         retry
 
       ensure
         # Remove só se existir para evitar remover indevidamente
-        mutex.synchronize do
+        mutex_subscribed_channels.synchronize do
           if subscribed_channels.key?(channel)
             subscribed_channels.delete(channel)
             logger.info "❌ Subscrição ao canal '#{channel}' foi encerrada."
@@ -61,9 +55,5 @@ class RedisSubscriberService
         end
       end
     end
-  end
-
-  def self.subscribed_channels
-    @subscribed_channels ||= {}
   end
 end
