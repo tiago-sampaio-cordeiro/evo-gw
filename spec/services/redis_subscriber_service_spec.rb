deab71e69c9a13d3911203d7e0a3b0a22fbccb67
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-06-02 18:00:22 -0300

    [DEV-174]: TEST: alterando testes

--- spec/services/redis_subscriber_service_spec.rb
+++ spec/services/redis_subscriber_service_spec.rb
@@ -16,21 +16,14 @@ RSpec.describe RedisSubscriberService do
 
   before do
     allow(Redis).to receive(:new).and_return(redis_mock)
-    allow(redis_mock).to receive(:subscribe).and_yield(subscription_callback)
     allow(Devices::Sender).to receive(:send)
-  end
-
-  let(:subscription_callback) do
-    double('SubscriptionCallback').tap do |cb|
-      allow(cb).to receive(:message).and_yield(channel, message)
-    end
   end
 
   context 'quando o canal já está inscrito' do
     it 'não inicia nova thread' do
       mutex.synchronize { subscribed_channels[channel] = true }
 
-      result = RedisSubscriberService.start(
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -38,6 +31,7 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
+      result = service.start
       expect(result).to be_nil
     end
   end
@@ -46,7 +40,13 @@ RSpec.describe RedisSubscriberService do
     let(:message) { { cmd: 'notify', args: ['arg1'] }.to_json }
 
     it 'envia comando via Devices::Sender' do
-      thread = RedisSubscriberService.start(
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -54,7 +54,8 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
-      thread.join
+      thread = service.start
+      thread.join(0.1)
 
       expect(Devices::Sender).to have_received(:send).with(ws, 'notify', 'arg1')
       expect(logger).to have_received(:debug).with(/Mensagem recebida/)
@@ -65,7 +66,13 @@ RSpec.describe RedisSubscriberService do
     let(:message) { 'INVALID_JSON' }
 
     it 'loga erro de parse' do
-      thread = RedisSubscriberService.start(
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -73,7 +80,8 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
-      thread.join
+      thread = service.start
+      thread.join(0.1)
 
       expect(logger).to have_received(:error).with(/Erro ao fazer parse/)
     end
@@ -83,7 +91,13 @@ RSpec.describe RedisSubscriberService do
     let(:message) { { something_else: true }.to_json }
 
     it 'loga warning e ignora' do
-      thread = RedisSubscriberService.start(
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -91,7 +105,8 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
-      thread.join
+      thread = service.start
+      thread.join(0.1)
 
       expect(logger).to have_received(:warn).with(/Payload recebido sem comando/)
       expect(Devices::Sender).not_to have_received(:send)
@@ -106,7 +121,13 @@ RSpec.describe RedisSubscriberService do
     end
 
     it 'loga erro e não interrompe' do
-      thread = RedisSubscriberService.start(
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -114,7 +135,8 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
-      thread.join
+      thread = service.start
+      thread.join(0.1)
 
       expect(logger).to have_received(:error).with(/Erro ao processar comando/)
     end
@@ -128,7 +150,13 @@ RSpec.describe RedisSubscriberService do
     let(:message) { { cmd: 'noop' }.to_json }
 
     it 'loga erro de conexão' do
-      thread = RedisSubscriberService.start(
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      service = RedisSubscriberService.new(
         channel: channel,
         ws: ws,
         mutex_subscribed_channels: mutex,
@@ -136,9 +164,88 @@ RSpec.describe RedisSubscriberService do
         subscribed_channels: subscribed_channels
       )
 
-      thread.join
+      thread = service.start
+      thread.join(0.1)
 
       expect(logger).to have_received(:error).with(/Falha ao conectar ao Redis/)
+    end
+  end
+
+  class TestLogger
+    attr_reader :messages
+
+    def info(msg)
+      @messages << "INFO: #{msg}"
+    end
+
+    def warn(msg)
+      @messages << "WARN: #{msg}"
+    end
+
+    def debug(msg)
+      @messages << "DEBUG: #{msg}"
+    end
+
+    def initialize
+      @messages = []
+    end
+
+    def error(msg)
+      @messages << msg
+    end
+  end
+
+  context 'quando ocorre um erro inesperado no Redis' do
+    let(:fake_error) { StandardError.new('erro aleatório') }
+    let(:test_logger) { TestLogger.new }
+
+    before do
+      allow(Kernel).to receive(:sleep).with(2)
+      allow(Redis).to receive(:new).and_raise(fake_error)
+    end
+
+    it 'loga o erro e tenta reconectar' do
+      service = RedisSubscriberService.new(
+        channel: channel,
+        ws: ws,
+        mutex_subscribed_channels: mutex,
+        logger: test_logger,
+        subscribed_channels: subscribed_channels
+      )
+      thread = service.start
+      thread.join(0.1)
+
+      expect(thread).to be_a(Thread)
+      expect(test_logger.messages.any? { |m| m =~ /Erro no Redis \(canal: #{channel}\): erro aleatório/ }).to be true
+    end
+  end
+
+  context 'quando uma exceção é lançada fora do Devices::Sender' do
+    let(:message) { { cmd: 'unexpected_error', args: [] }.to_json }
+
+    before do
+      allow(redis_mock).to receive(:subscribe).and_yield(
+        double('SubscriptionBlock').tap do |cb|
+          allow(cb).to receive(:message).and_yield(channel, message)
+        end
+      )
+
+      allow(Devices::Sender).to receive(:send).and_raise(RuntimeError, 'Erro inesperado')
+    end
+
+    it 'captura e loga o erro' do
+      service = RedisSubscriberService.new(
+        channel: channel,
+        ws: ws,
+        mutex_subscribed_channels: mutex,
+        logger: logger,
+        subscribed_channels: subscribed_channels
+      )
+
+      thread = service.start
+      thread.join(0.1)
+
+      expect(logger).to have_received(:error).with(/Erro ao processar comando/)
     end
   end
 end
